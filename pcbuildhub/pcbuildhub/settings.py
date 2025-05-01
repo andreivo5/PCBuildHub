117c609cb438c04f5eb09a6f6f56e2f8c69c8746
andreivO5 <andreiv529@gmail.com>
2025-05-01 16:11:31 +0100

    added logging config

--- pcbuildhub/pcbuildhub/settings.py
+++ pcbuildhub/pcbuildhub/settings.py
@@ -131,3 +131,23 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'pcbuildhub.log'),
+        },
+    },
+    'root': {
+        'handlers': ['console', 'file'],
+        'level': 'DEBUG',
+    },
+}
