19251a0f00a38ecc73d5e5716b6e3156e0e95e0b
andreivO5 <andreiv529@gmail.com>
2025-05-01 16:17:45 +0100

    small fix to settings.py

--- pcbuildhub/pcbuildhub/settings.py
+++ pcbuildhub/pcbuildhub/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main', 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
